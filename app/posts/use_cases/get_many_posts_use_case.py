74a2759bb797c9c3f3991a6e2e7fbe8fd2222d66
Filipi <flf.2008brasil@hotmail.com>
2025-09-25 15:07:21 -0300

    partially finished

--- app/posts/use_cases/get_many_posts_use_case.py
+++ app/posts/use_cases/get_many_posts_use_case.py
@@ -1,26 +1,37 @@
+import asyncio
+from typing import List
+
 from app.http_request.http_request import http_request
+
+
+async def handle_fetch(url: str):
+
+    allResults = []
+
+    try:
+        response = await http_request(
+            url=url,
+            method="get",
+        )
+        results = response.json()
+        if len(results) > 0:
+            allResults.append(results)
+    except Exception as e:
+        print(f"Failed to get url: {url}", e)
+        return None
+    return allResults
 
 
 async def get_many_posts_use_case():
 
     urls = [
         "https://jsonplaceholder.typicode.com/posts/1",
-        "http://jsonplaceholder.typiode.com/2",
+        "https://jsonplaceholder.typicode.com/posts/2",
         "https://jsonplaceholder.typicode.com/posts/3",
     ]
 
-    allResults = []
+    allRequests = [handle_fetch(url) for url in urls]
 
-    for url in urls:
-        try:
-            response = await http_request(
-                url=url,
-                method="get",
-            )
-            results = response.json()
-            if len(results) > 0:
-                allResults.append(results)
-        except Exception as e:
-            print(f"Failed to get url: {url}", e)
+    results = await asyncio.gather(*allRequests)
 
-    return allResults
+    return results
